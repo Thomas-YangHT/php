2d59be0e9ac2e6c0f955297299da5cbe53b5d6eb
yanght <168447636@qq.com>
2020-05-08 07:55:21 +0800

    2

--- wechatphp/index.php
+++ wechatphp/index.php
@@ -9,34 +9,33 @@ define("TOKEN", "weixin");
 
 $help="请输入要查询的命令：\n===================\n如: ls ---返回用法链接\n";
 $help.="001-<a href='https://mubu.com/doc/3xyI7zD_Yo'>CmdCheetSheet</a>\n";
-$help.="002-<a href='https://mubu.com/doc/3y4NwBXCxo'>简述shell流程控制</a>\n";
 $help.="003-<a href='https://mubu.com/doc/2-KsPtqeKo'>国内安装源总结</a>\n";
-$help.="004-<a href='https://mubu.com/doc/2d8bwNidNo'>简述网络排障</a>\n";	
-$help.="005-<a href='https://mubu.com/doc/2wPYi23fso'>简述运维操作工具</a>\n";	
-$help.="006-<a href='https://mubu.com/doc/3Q14_dh4Go'>markdown文档工具</a>\n";	
-$help.="007-<a href='https://mubu.com/doc/3FM8gqgzro'>简述web应用性能优化</a>\n";	
-$help.="008-<a href='https://mubu.com/doc/3DOZgQxGwo'>简述微信公众号开发</a>\n";	
 $help.="009-<a href='https://mubu.com/doc/3uK_TGfrXo'>解决故障[更新中]</a>\n";
+$help.="002-<a href='https://mubu.com/doc/3y4NwBXCxo'>简述shell流程控制</a>\n";
+$help.="004-<a href='https://mubu.com/doc/2d8bwNidNo'>简述网络排障</a>\n";
+$help.="005-<a href='https://mubu.com/doc/2wPYi23fso'>简述运维操作工具</a>\n";
+$help.="007-<a href='https://mubu.com/doc/3FM8gqgzro'>简述web应用性能优化</a>\n";
+$help.="008-<a href='https://mubu.com/doc/3DOZgQxGwo'>简述微信公众号开发</a>\n";
+$help.="006-<a href='https://mubu.com/doc/3Q14_dh4Go'>markdown文档工具</a>\n";
 $help.="010-<a href='https://mubu.com/doc/4pNs1XBo_f9'>安全扫描</a>\n";
 $help.="011-<a href='https://mubu.com/doc/3Bxi3I9DCo'>Raspberry pi4使用记录</a>\n";
 $help.="012-<a href='https://mubu.com/doc/1XiLnBztCo'>K8S启动盘使用帮助</a>\n";
-$help.="\n待完成waiting：\n";
-$help.="013-<a href=''>试用pandas分析性能数据</a>\n";
-$help.="014-<a href=''>性能优化</a>\n";
-$help.="015-<a href=''>Python实例</a>\n";
-$help.="016-<a href=''>日志收集系统</a>\n";
-$help.="017-<a href=''>来自编个监控系统</a>\n";
-$help.="018-<a href=''>从win10到centos7</a>\n";
 $help.="\nCloudMan 每天5分钟系列：\n";
 $help.="<a href='https://mp.weixin.qq.com/s/7o8QxGydMTUe4Q7Tz46Diw'>[Docker教程]</a>\n";
 $help.="<a href='https://mp.weixin.qq.com/s/RK6DDc8AUBklsUS7rssW2w'>[Kubernetes教程]</a>\n";
 $help.="<a href='https://mp.weixin.qq.com/s/QtdMkt9giEEnvFTQzO9u7g'>[OpenStack教程]</a>\n";
 $help.="\n其它：\n";
-$help.="001-<a href='https://mubu.com/doc/3u65WbvQsp'>SimpleComputerWords</a>\n";		
-$help.="002-<a href='https://mubu.com/doc/3mtscGgyIo'>简述测试概念与工具</a>\n";			
+$help.="001-<a href='https://mubu.com/doc/3u65WbvQsp'>SimpleComputerWords</a>\n";
+$help.="002-<a href='https://mubu.com/doc/3mtscGgyIo'>简述测试概念与工具</a>\n";
 $help.="003-<a href='https://mubu.com/doc/LYdGMKtto'>IT架构图</a>\n";
 $help.="004-<a href='https://mp.weixin.qq.com/mp/homepage?__biz=Mzg4MjAyMDgzMQ==&hid=1&sn=ce7139573c267c56ae45f026c4242045'>LinuxMan往期目录</a>\n";
-		
+$help.="\n待完成waiting：\n";
+$help.="013-<a href=''>试用pandas分析数据</a>\n";
+$help.="014-<a href=''>性能优化</a>\n";
+$help.="015-<a href=''>Python实例</a>\n";
+$help.="016-<a href=''>日志收集系统</a>\n";
+$help.="017-<a href=''>来编个监控系统</a>\n";
+
 $wechatObj = new wechatCallbackapiTest();
 if (!isset($_GET['echostr'])) {
     $wechatObj->responseMsg();
@@ -87,7 +86,8 @@ class wechatCallbackapiTest
                     break;
                 case "text":
                     if (strstr($postObj->Content, "第三方")){
-                        $result = $this->relayPart3("http://www.fangbei.org/test.php".'?'.$_SERVER['QUERY_STRING'], $postStr);
+                      #  $result = $this->relayPart3("http://www.fangbei.org/test.php".'?'.$_SERVER['QUERY_STRING'], $postStr);
+											  $result = "";
                     }else{
                         $result = $this->receiveText($postObj);
                     }
@@ -219,7 +219,7 @@ class wechatCallbackapiTest
 
         //自动回复模式
 
-				
+
 		if (strstr($keyword, "help") || strstr($keyword,"?")){
 			global $help;
             $content = $help;
